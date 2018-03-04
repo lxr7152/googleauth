60ec3e987009eaa663770edb16c785f7e3dafd32
lvchao <lvchao@didichuxing.com>
2018-03-04 11:11:24 +0800

    master 第二次提交

--- src/main/java/com/auth/google/Mtest.java
+++ src/main/java/com/auth/google/Mtest.java
@@ -5,5 +5,6 @@ public class Mtest {
 	public static void main(String[] args) {
 		 System.out.println("test……");
 		 System.out.println("test……mater");
+		 System.out.println("test……mater2");
 	}
 }
