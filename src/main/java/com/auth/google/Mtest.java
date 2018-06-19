a13af167fe4c39cd0be1a340afc9dd4b829d8345
lvchao <lvchao@didichuxing.com>
2018-06-19 20:33:31 +0800

    test

--- src/main/java/com/auth/google/Mtest.java
+++ src/main/java/com/auth/google/Mtest.java
@@ -4,7 +4,5 @@ public class Mtest {
 
 	public static void main(String[] args) {
 		 System.out.println("test……");
-		 System.out.println("test……mater");
-		 System.out.println("test……mater2");
 	}
 }
