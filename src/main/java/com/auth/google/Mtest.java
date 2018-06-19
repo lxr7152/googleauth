64e5533e9f0934dc308d62acc0195a7fc5eb6ea2
lvchao <lvchao@didichuxing.com>
2018-06-19 20:54:25 +0800

    e

--- src/main/java/com/auth/google/Mtest.java
+++ src/main/java/com/auth/google/Mtest.java
@@ -3,7 +3,7 @@ package com.auth.google;
 public class Mtest {
 
 	public static void main(String[] args) {
-		 System.out.println("test23……");
-		 
+		 System.out.println("test……");
+		 System.out.println("test123……");
 	}
 }
